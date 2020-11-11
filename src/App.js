e3509f7ca78e85ecd26e3a595af2b6b26e7b255e
Abdelwahab0 <abdelwahabahmed0@outlook.com>
2020-11-11 15:49:01 +0200

    Implemented protected routes, also created custom hook for getting the userAuth data

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
+import { IsUserRedirect, ProtectedRoute } from './helpers/routes';
 
 import * as ROUTES from './constants/routes';
 import { Home, Browse, SignIn, SignUp } from './pages';
-
+import { useAuthListener } from './hooks';
 
 export default function App() {
+  const { user } = useAuthListener();
+
   return (
     <Router>
-      <Route exact path={ROUTES.HOME}>
-        <Home />
-      </Route>
-      <Route exact path={ROUTES.BROWSE}>
-        <Browse />
-      </Route>
-      <Route exact path={ROUTES.SIGN_IN}>
-        <SignIn />
-      </Route>
-      <Route exact path={ROUTES.SIGN_UP}>
-        <SignUp />
-      </Route>
+      <Switch>
+        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_IN}>
+          <SignIn />
+        </IsUserRedirect>
 
+        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_UP}>
+          <SignUp />
+        </IsUserRedirect>
+
+        <ProtectedRoute user={user} path={ROUTES.BROWSE}>
+          <Browse />
+        </ProtectedRoute>
+
+        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.HOME} exact>
+          <Home />
+        </IsUserRedirect>
+      </Switch>
     </Router>
   );
 }
