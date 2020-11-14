0eccf2e5768d85b65dc5a098bc02979dd186f493
Abdelwahab0 <abdelwahabahmed0@outlook.com>
2020-11-14 12:11:53 +0200

    Adding Roboto font

--- src/global-styles.js
+++ src/global-styles.js
@@ -2,7 +2,6 @@ import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyles = createGlobalStyle`
   html, body {
-  @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap');
   font-family: 'Roboto', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
